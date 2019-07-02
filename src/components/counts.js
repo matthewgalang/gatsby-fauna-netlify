4d4c621baa8d711b2a395373c5fc3f5bf520d337
Matt Galang <magalang1@gmail.com>
2019-07-02 15:49:52 -0700

    add odometer and correct cups calculation

--- src/components/counts.js
+++ src/components/counts.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import api from '../utils/api';
 import isLocalHost from '../utils/isLocalHost';
+import './odometer-theme-minimal.css';
+import Odometer from "react-odometerjs";
 
 export default class Counts extends Component {
     state = {
         "stackoverflow.com":"",
-        coffeeCupsCount: "",
-        codeLinesCount: ""
+        coffeeCupsCount: 0,
+        codeLinesCount: "",
+        odometerValue:0
     }
 
     componentDidMount() {
         // Fetch all counts
+        
+        this.updateCups()
         api.readAll().then((counts) => {
             if (counts.message === 'unauthorized') {
                 if (isLocalHost()) {
@@ -20,28 +25,65 @@ export default class Counts extends Component {
                 }
                 return false
             }
-            console.log('state', this.state)
+            
             this.setState(Object.fromEntries(counts.map((count) => [count.data.title,count.data.count])))
+            console.log('state', this.state)
+        })
+        // const intervalId = setInterval(() => recurringFunction(), 10000)
+        // this.setState({ intervalId })
+    }
+    updateCups(){
+        const today = new Date()
+        const firstcup = new Date('2017-01-23T08:00:00')
+        const totaldaydiff = Math.ceil(Math.abs(today.getTime() - firstcup.getTime())/(1000*60*60*24))
+        var workdays = totaldaydiff - (Math.floor(totaldaydiff/7) * 2)
+        const todayDay = today.getDay()
+        const firstcupDay = firstcup.getDay()
+        if (firstcupDay - todayDay > 1) {
+            workdays = workdays - 2
+        }
+        if (firstcupDay === 0 && todayDay !== 6) {
+            workdays = workdays - 1
+        }
+        if (firstcupDay === 6 && todayDay !== 0) {
+            workdays = workdays - 1
+        }
+        this.setState({
+            coffeeCupsCount: workdays
         })
     }
     renderVisits() {
         return(
         <div key="visits">
-            Visited stackoverflow {this.state["stackoverflow.com"]} times.
+            <span>Visited stackoverflow <Odometer 
+                format="d" 
+                duration={10000} 
+                value={ this.state["stackoverflow.com"] ? this.state["stackoverflow.com"] : this.state.odometerValue }/> times.
+            </span>
         </div>
         )
     }
     renderCups() {
         return(
         <div key="cups">
-            { this.state.coffeeCupsCount } cups of coffee brewed at M4S.
+            <span>
+                <Odometer 
+                format="d" 
+                duration={10000} 
+                value={ this.state.coffeeCupsCount ? this.state.coffeeCupsCount : this.state.odometerValue }/> cups of coffee brewed at M4S.
+            </span>
         </div>
         )
     }
     renderLines() {
         return(
         <div key="lines">
-            { this.state.codeLinesCount } lines of code.
+            <span>
+                <Odometer 
+                format="d" 
+                duration={10000} 
+                value={ this.state.codeLinesCount ? this.state.codeLinesCount : this.state.odometerValue }/> lines of code.
+            </span>
         </div>
         )
     }
